c610599bc3aec2b4bf897bee5891275113fc6ef6
Nevoska <matheus_a.neves@hotmail.com>
2020-05-30 18:58:16 -0300

    test ok'122

--- app/src/main/java/com/nasa/projectcesta/MainActivity.java
+++ app/src/main/java/com/nasa/projectcesta/MainActivity.java
@@ -2,16 +2,29 @@ package com.nasa.projectcesta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //salve armando
-        //ok
-        //ornacio
+        setContentView(R.layout.menu);
+
     }
+
+    public void irCadastro(View view) {
+        Intent intent = new Intent(this, cadastro.class);
+        startActivity(intent);
+    }
+
+    public void irLogin(View view) {
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/nasa/projectcesta/Tipo.java
@@ -0,0 +1,17 @@
+package com.nasa.projectcesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Tipo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tipo);
+    }
+
+}
