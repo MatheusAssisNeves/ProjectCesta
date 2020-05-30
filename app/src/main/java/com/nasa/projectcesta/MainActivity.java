c58e4c6eac89d2759942ae75ed2787987303cef7
Nevoska <matheus_a.neves@hotmail.com>
2020-05-30 16:38:03 -0300

    cc

--- app/src/main/java/com/nasa/projectcesta/MainActivity.java
+++ app/src/main/java/com/nasa/projectcesta/MainActivity.java
@@ -3,6 +3,7 @@ package com.nasa.projectcesta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
-//kdkdk
+
+    public void onClick(View v) {
+    }
