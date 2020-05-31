0ed9a1b1b7dd05ccabc63b0113e02f3c034d332f
Nevoska <matheus_a.neves@hotmail.com>
2020-05-31 02:36:11 -0300

    test ok'122

--- app/src/main/java/com/nasa/projectcesta/finalDoar.java
+++ app/src/main/java/com/nasa/projectcesta/finalDoar.java
@@ -11,7 +11,6 @@ import android.view.View;
 
 
 public class finalDoar extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
