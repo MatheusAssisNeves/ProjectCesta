5625c3b1c5afcfe1eea63c9801f07952ee9629f8
joaov <joao.victor.pacheco@outlook.com>
2020-05-30 21:15:01 -0300

    salve

---
+++ app/src/main/java/com/nasa/projectcesta/Lider.java
@@ -0,0 +1,14 @@
+package com.nasa.projectcesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lider);
+    }
+}
